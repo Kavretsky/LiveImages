99cb62ee61e5e1938c2fdc506af5822daa9dcf2b
Nikolay Kavretsky <kavretsky@gmail.com>
2024-10-28 21:47:57 +0300

    Initial Commit

---
+++ LiveImages/LiveImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveImagesApp.swift
+//  LiveImages
+//
+//  Created by Nikolay Kavretsky on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LiveImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
